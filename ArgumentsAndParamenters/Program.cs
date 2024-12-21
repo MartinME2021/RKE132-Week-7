1cdc7944e20025e330688400c1bc00457132d895
MartinME2021 <martin.grensman@tktk.ee>
2024-12-21 15:54:12 +0200

    Add files via upload

---
+++ ArgumentsAndParamenters/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Enter a word.");
+string userInPut = Console.ReadLine();
+
+PrintAnyWord(userInPut);  //InPut - Argument
+
+static void PrintAnyWord(string anyString) //anyString - Parameter
+{
+    anyString = anyString.ToUpper();
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine(anyString);
+    }
+}
